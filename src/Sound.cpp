aa08b8b60a48fff1d9daf0f62a258900900b2357
Thomas Steinacher <tom@eggdrop.ch>
2012-08-17 21:44:46 -0700

    Workaround for crashes when accepting a call.

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -25,6 +25,10 @@ Sound::Sound()
 Sound::~Sound()
 {
     stop();
+    if (snd_port_) {
+        pjmedia_snd_port_destroy(snd_port_);
+        snd_port_ = NULL;
+    }
     if (pool_)
         pj_pool_release(pool_);
 }
@@ -54,18 +58,22 @@ void Sound::startRing()
             LogHandler::getInstance().log(LogInfo(LogInfo::STATUS_ERROR, "pjsip", status, "Error in pjmedia_wav_player_port_create"));
             return;
         }
-        status = pjmedia_snd_port_create_player(pool_, /* pool */
-                                                device_, /* use default dev. */
-                                                file_port_->info.clock_rate,/* clock rate. */
-                                                file_port_->info.channel_count,/* # of channels. */
-                                                file_port_->info.samples_per_frame, /* samples per frame. */
-                                                file_port_->info.bits_per_sample,/* bits per sample. */
-                                                0, /* options */
-                                                &snd_port_ /* returned port */
-                                                );
-        if (status != PJ_SUCCESS) {
-            LogHandler::getInstance().log(LogInfo(LogInfo::STATUS_ERROR, "pjsip", status, "Failed to create player"));
-            return;
+        
+        
+        if (!snd_port_) {
+            status = pjmedia_snd_port_create_player(pool_, /* pool */
+                                                    device_, /* use default dev. */
+                                                    file_port_->info.clock_rate,/* clock rate. */
+                                                    file_port_->info.channel_count,/* # of channels. */
+                                                    file_port_->info.samples_per_frame, /* samples per frame. */
+                                                    file_port_->info.bits_per_sample,/* bits per sample. */
+                                                    0, /* options */
+                                                    &snd_port_ /* returned port */
+                                                    );
+            if (status != PJ_SUCCESS) {
+                LogHandler::getInstance().log(LogInfo(LogInfo::STATUS_ERROR, "pjsip", status, "Failed to create player"));
+                return;
+            }
         }
         
         
@@ -86,16 +94,17 @@ void Sound::startDial()
 void Sound::setSoundDevice(const int device)
 {
     device_ = device;
+    if (snd_port_) {
+        pjmedia_snd_port_destroy(snd_port_);
+        snd_port_ = NULL;
+    }
 }
 
 //-----------------------------------------------------------------------------
 void Sound::stop()
 {
-    if (snd_port_) {
-        pjmedia_snd_port_disconnect(snd_port_);
-        pjmedia_snd_port_destroy(snd_port_);
-        snd_port_ = NULL;
-    }
+    // If we destroy the sound port here, we crash for some reason when accepting a call.
+    
     if (file_port_) {
         pjmedia_port_destroy(file_port_);
         file_port_ = NULL;
